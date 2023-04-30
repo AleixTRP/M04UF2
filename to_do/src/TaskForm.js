808ed10d9eda3e4af5f0f62bc02a469281b53823
AleixTRP <116338424+AleixTRP@users.noreply.github.com>
2023-04-30 17:39:19 +0200

    Update TaskForm.js

--- to_do/src/TaskForm.js
+++ to_do/src/TaskForm.js
@@ -3,52 +3,52 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
-class TaskForm extends React.Component
-{
-	
-		constructor(props){
-			super(props);
-				this.state= {
+ class TaskForm extends React.Component
+   {
+  
+          constructor(props){
+             super(props);
+                  this.state= {
+ 
+                  task: ""
+                  };
+          }
+          render(){
+          return (
+          <form>
+         <p><TextField variant="standard" input tpye="text" value={this.state.task} onChange={event => {
+ 
+          this.setState({
+              task:event.target.value
+ 
+              });
+          }}
+      placeholder="Escribe tu nueva tarea" sx = {{ margin: 3 }} />
+          <Button variant="contained" type="button" sx = {{ position: 'relative', top: '23px', right: '20px'}}onClick={() =>{
+           if (this.state.task.trim() === "") {
+               this.setState({
+                  task: ""
+                  });
+                  return;
+             }
+ 
+          this.props.onAddTask(this.state.task);
+ 
+          this.setState({
+              task:""
+              });
 
-				task: ""
-				};
-		}
-		render(){
-		return (
-		<form>
-		<p><TextField variant="standard" input tpye="text" value={this.state.task} onChange={event => {  
-
-		this.setState({
-			task:event.target.value
-			
-			});
-		}}
-    placeholder="Escribe una tarea" sx = {{ margin: 4 }} />
-		<Button variant="contained" type="button" sx = {{ position: 'relative', top: '23px'}}onClick={() =>{	
-		 if(this.state.task.trim() == ""){
-      		 this.setState({
-	        	task: ""
-	         	});
-		        return;
-		   }
-
-		this.props.onAddTask(this.state.task);
-
-		this.setState({
-			task:""
-			});
-		
-		}}>+</Button></p>
-		</form>
-
-
-		);
-
-	}
-
-}
-
-export default TaskForm;
+          }}>Añadir</Button></p>
+         </form>
+ 
+ 
+         );
+ 
+     }
+ 
+  }
+ 
+  export default TaskForm;
 
 
 
