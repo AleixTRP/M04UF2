259b0b7c0bdedf3cd3ac3d7da88ea9235d75c472
AleixTRP <116338424+AleixTRP@users.noreply.github.com>
2023-05-11 20:59:47 +0200

    Update TaskList.js

--- to_do/src/TaskList.js
+++ to_do/src/TaskList.js
@@ -1,49 +1,39 @@
 import React from 'react';
+import TaskItem from './TaskItem'
+import List from '@mui/material/List'
 
-import TaskItem from './TaskItem';
-import List from '@mui/material/List';
+class TaskList extends React.Component {
 
-class TaskList extends React.Component
-  {
-       constructor (props){
-           super(props);
- 
-             this.state = {
-                  items: this.props.list
-              };
-         }
-         render(){
-          console.log("render");
-          let counter = -1;
-          const tasks = this.state.items.map(task => {
-          counter++;
-          return (
-              <TaskItem text={task} num_task={counter} onDeleteTask= {this.props.onDeleteTask} />
-              );
-          });
-      return (
- 
-          <List>
-          {tasks}
-          </List>
- 
- 
-          );
- 
-      }
-  }
- 
- 
-  export default TaskList;
+	constructor(props) {
+		super(props);
+	}
 
+	itemList = () => {
+		
+		let counter = -1;
+		let tasks = this.props.list.map(task => {
+			counter++;
+			return (
+				<TaskItem key={counter} text={task} time={this.props.listTime[counter]} onDeleteTask={this.props.onDeleteTask}/>
+			);
+		});
 
+		return tasks;
+	}
 
+	render() {
+		
+		const tasks = this.itemList();
 
+		return (
+		
+		<List>
+			{tasks}
+		</List>
+		);
+	}
+}
 
-
-
-
-
-
+export default TaskList;
 
 
